715bb9a287b328e5dfed4a7719f637efad745f71
zh <779149549@qq.com>
2019-11-07 00:35:39 +0800

    示例图1 完整界面代码

---
+++ 界面设计(1)/packages/MaterialDesignThemes.2.6.0/src/net45/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace MaterialDesignThemes.Wpf.Converters
+{
+    public sealed class BooleanToVisibilityConverter : BooleanConverter<Visibility>
+    {
+        public BooleanToVisibilityConverter() :
+            base(Visibility.Visible, Visibility.Collapsed)
+        { }
+    }
+}
---
+++ 界面设计(1)/packages/MaterialDesignThemes.2.6.0/src/net45/Converters/ShadowConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Effects;
+
+namespace MaterialDesignThemes.Wpf.Converters
+{
+    public class ShadowConverter : IValueConverter
+    {
+        public static readonly ShadowConverter Instance = new ShadowConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is ShadowDepth)) return null;
+
+            return Clone(Convert((ShadowDepth)value));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static DropShadowEffect Convert(ShadowDepth shadowDepth)
+        {
+            return ShadowInfo.GetDropShadow(shadowDepth);
+        }
+
+        private static DropShadowEffect Clone(DropShadowEffect dropShadowEffect)
+        {
+            if (dropShadowEffect == null) return null;
+            return new DropShadowEffect()
+            {
+                BlurRadius = dropShadowEffect.BlurRadius,
+                Color = dropShadowEffect.Color,
+                Direction = dropShadowEffect.Direction,
+                Opacity = dropShadowEffect.Opacity,
+                RenderingBias = dropShadowEffect.RenderingBias,
+                ShadowDepth = dropShadowEffect.ShadowDepth
+            };
+        }
+    }
+}
