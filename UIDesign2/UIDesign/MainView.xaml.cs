0cde13746f89ce86d13620c6a2fa8f29fa42e0fa
zh <779149549@qq.com>
2020-03-25 21:19:39 +0800

    工业样例图

---
+++ UIDesign2/UIDesign/MainView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using UIDesign.ViewModel;
+
+namespace UIDesign
+{
+    /// <summary>
+    /// MainView.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+            this.DataContext = new MainViewModels();
+        }
+    }
+}
